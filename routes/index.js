04ffec6facc766aa5e6c0205fdf0630cda25b8c4
Jim <wefactoryou@gmail.com>
2017-10-21 06:30:04 -0700

    create about and contact pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DevPupil | IDLE' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About DevPupil'});
+});
+
+/* GET contact page. */
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'Contact DevPupil'});
+  })
+  .post(function(req, res, next) {
+    res.render('thank', { title: 'Thank You!'});
+  });
 
 module.exports = router;
