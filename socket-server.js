9eff26a2673265af992510f850da538dff258578
Jim <wefactoryou@gmail.com>
2017-10-22 22:07:36 -0700

    save tasks to database

--- socket-server.js
+++ socket-server.js
@@ -6,14 +6,19 @@ var roomList = {};
 
 module.exports = function(server) {
   var str = 'Time to start coding! \n\n' +
-            'name = "Your Name"';
+            'name = "Your name"';
 
   var io = socketIO(server);
   io.on('connection', function(socket) {
     socket.on('joinRoom', function(data) {
       if (!roomList[data.room]) {
         var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb) {
-          cb(true);
+          var self = this;
+          Task.findByIdAndUpdate(data.room, {content: self.document}, function(err) {
+            if (err) return cb(false);
+            cb(true);
+          });
+
         });
         roomList[data.room] = socketIOServer;
       }
